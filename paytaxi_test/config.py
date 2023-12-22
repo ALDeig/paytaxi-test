dcac46691e0a9fde343142190955422fcdc4d023
renovatio <alkin.denis@gmail.com>
2023-12-22 18:25:49 +0400

    fix: udpate database creds

--- paytaxi_test/config.py
+++ paytaxi_test/config.py
@@ -5,17 +5,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf8")
 
-    db_user: str
-    db_password: str
-    db_name: str
-    db_host: str
-    db_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
 
     @property
     def db_url(self) -> PostgresDsn:
         return PostgresDsn(
-            f"postgresql+asyncpg://{self.db_user}:{self.db_password}@"
-            f"{self.db_host}:{self.db_port}/{self.db_name}"
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
+            f"{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
         )
     
 
